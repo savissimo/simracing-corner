6ad7a298a50e43a03de97a3fa9b0702cfc436515
savissimo <simone.saviolo@gmail.com>
2021-06-10 23:40:39 +0200

    Fixed navbar dropdown

--- docs/assets/js/scripts/navbar.js
+++ docs/assets/js/scripts/navbar.js
@@ -1,8 +1,25 @@
 document.querySelectorAll('.navbar-item').forEach(($navbarItem) => {
 	const $childrenItemsContainer = $navbarItem.querySelector('.navbar-item-children')
-	$navbarItem.addEventListener('click', (e) => {
+	if (!$childrenItemsContainer) {
+		return
+	}
+	const $navbarLink = $navbarItem.querySelector('.navbar-link')
+	$navbarLink.addEventListener('click', (e) => {
 		$childrenItemsContainer.classList.toggle('open')
 		e.stopPropagation()
 		return false
+	})
+})
+
+document.addEventListener('click', (e) => {
+	document.querySelectorAll('.navbar-item').forEach(($navbarItem) => {
+		const $childrenItemsContainer = $navbarItem.querySelector('.navbar-item-children')
+		if (!$childrenItemsContainer) {
+			return
+		}
+		if (!$navbarItem.contains(e.target)) {
+			console.log('closing', $navbarItem)
+			$childrenItemsContainer.classList.remove('open')
+		}
 	})
 })
