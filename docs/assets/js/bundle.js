5af5b861ce04b0bcc330fda2952906ffcdc4e61d
savissimo <simone.saviolo@gmail.com>
2021-06-10 19:39:59 +0200

    Fix bundle JS

--- docs/assets/js/bundle.js
+++ docs/assets/js/bundle.js
@@ -5,9 +5,9 @@ dir_path: scripts/
 scripts:
   - navbar.js
 ---
-{% for script in scripts %}
-    {% assign script_path = dir_path | append: script %}
+{% for script in page.scripts %}
+    {% assign script_path = page.dir_path | append: script %}
     (function () {
-        {% include_relative script_path %}
+        {% include_relative {{script_path}} %}
     })();
 {% endfor %}
