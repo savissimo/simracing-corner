f66ed2eeb8df95acd6642b8db4b1c099b14a0229
savissimo <simone.saviolo@gmail.com>
2021-06-13 15:22:35 +0200

    Picture gallery now works even when picking pictures from the list

--- docs/assets/js/scripts/picture-gallery.js
+++ docs/assets/js/scripts/picture-gallery.js
@@ -8,6 +8,7 @@ function movePictureToFrontWindow($i_picture, $i_frontWindow, $i_prevPictureBtn,
 		$i_frontWindow.appendChild($i_picture.cloneNode(true))
 
 		const pictureIndex = $i_pictures.findIndex($p => $p === $i_picture)
+		console.log('now showing ' + pictureIndex + '/' + $i_pictures.length)
 		if (pictureIndex < 0) {
 			$i_prevPictureBtn && ($i_prevPictureBtn.style.display = 'none')
 			$i_nextPictureBtn && ($i_nextPictureBtn.style.display = 'none')
@@ -23,15 +24,19 @@ function movePictureToFrontWindow($i_picture, $i_frontWindow, $i_prevPictureBtn,
 				$i_prevPictureBtn.style.display = 'block'
 				$i_prevPictureBtn.replaceWith($newPrevButton) // removes all previously registered event listeners
 				$newPrevButton.addEventListener('click', function(e) {
+					console.log(' ==> moving to ' + prevIndex + '/' + $i_pictures.length)
 					movePictureToFrontWindow($i_pictures[prevIndex], $i_frontWindow, $newPrevButton, $newNextButton, $i_pictures)
 				})
+				console.log('prev now moves to ' + prevIndex + '/' + $i_pictures.length)
 			}
 			if ($i_nextPictureBtn) {
 				$i_nextPictureBtn.style.display = 'block'
 				$i_nextPictureBtn.replaceWith($newNextButton) // removes all previously registered event listeners
 				$newNextButton.addEventListener('click', function(e) {
+					console.log(' ==> moving to ' + nextIndex + '/' + $i_pictures.length)
 					movePictureToFrontWindow($i_pictures[nextIndex], $i_frontWindow, $newPrevButton, $newNextButton, $i_pictures)
 				})
+				console.log('next now moves to ' + nextIndex + '/' + $i_pictures.length)
 			}
 		}
 	}
@@ -46,16 +51,17 @@ document.querySelectorAll('.picture-gallery[data-interactive]').forEach(($galler
 	if (!$frontWindow) {
 		return
 	}
-
-	$prevPictureBtn = $gallery.querySelector('.picture-gallery__control[data-action="prev"]')
-	$nextPictureBtn = $gallery.querySelector('.picture-gallery__control[data-action="next"]')
-
+	
 	const $pictures = [].slice.call($gallery.querySelectorAll('.picture-gallery__pictures > .picture-gallery__picture'))
 	if ($pictures.length > 0) {
+		const $prevPictureBtn = $gallery.querySelector('.picture-gallery__control[data-action="prev"]')
+		const $nextPictureBtn = $gallery.querySelector('.picture-gallery__control[data-action="next"]')
 		movePictureToFrontWindow($pictures[0], $frontWindow, $prevPictureBtn, $nextPictureBtn, $pictures)
 	}
 	$pictures.forEach(($picture, $i) => {
 		$picture.addEventListener('click', function(e) {
+			const $prevPictureBtn = $gallery.querySelector('.picture-gallery__control[data-action="prev"]')
+			const $nextPictureBtn = $gallery.querySelector('.picture-gallery__control[data-action="next"]')
 			movePictureToFrontWindow($picture, $frontWindow, $prevPictureBtn, $nextPictureBtn, $pictures)
 		})
 	})
